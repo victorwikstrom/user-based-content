74e1a31d687d0e7204556ffe47c3204fd981ce28
HBerglund <herman.e.berglund@gmail.com>
2021-04-20 11:41:33 +0200

    added section component to wrap the app in

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import AppTheme from "./theming/AppTheme";
+import Container from "./components/Container";
 
 ReactDOM.render(
   <React.StrictMode>
     <AppTheme>
-      <App />
+      <Container>
+        <App />
+      </Container>
     </AppTheme>
   </React.StrictMode>,
   document.getElementById("root")
