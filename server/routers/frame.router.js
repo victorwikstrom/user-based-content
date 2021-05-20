e312af5285fb711ad94140daaafac3be04dacf79
HBerglund <herman.e.berglund@gmail.com>
2021-05-20 14:21:31 +0200

    added a few security checks

--- server/routers/frame.router.js
+++ server/routers/frame.router.js
@@ -37,15 +37,29 @@ frameRouter.post("/api/frames", async (req, res) => {
 
 // FIND BY ID AND UPDATE
 frameRouter.put("/api/frames/:id", async (req, res) => {
+  //check if user is logged in
   if (!userIsLoggedIn(req)) {
     res.status(500).json("You need to log in before you can update");
     return;
   }
+
+  //check if user has admin rights
+  const adminUser = await userIsAdmin(req);
+  if (adminUser) {
+    const frame = await FrameModel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(frame);
+    return;
+  }
+
+  //check if user is author of the frame
   const userIsAuthorised = await userIsAuthor(req);
   if (!userIsAuthorised) {
     res.status(403).json("You may only edit your own frames");
     return;
   }
+
   const frame = await FrameModel.findByIdAndUpdate(req.params.id, req.body);
   if (!frame) {
     res.status(404).json("No frame was found");
@@ -56,15 +70,27 @@ frameRouter.put("/api/frames/:id", async (req, res) => {
 
 // FIND BY ID AND DELETE
 frameRouter.delete("/api/frames/:id", async (req, res) => {
+  //check if user is logged in
   if (!userIsLoggedIn(req)) {
     res.status(500).json("You need to log in before you can delete");
     return;
   }
+
+  //check if user has admin rights
+  const adminUser = await userIsAdmin(req);
+  if (adminUser) {
+    const frame = await FrameModel.findByIdAndDelete(req.params.id);
+    res.status(200).json(frame);
+    return;
+  }
+
+  //check if user is author of the frame
   const userIsAuthorised = await userIsAuthor(req);
   if (!userIsAuthorised) {
     res.status(403).json("You may only delete your own frames");
     return;
   }
+
   const frame = await FrameModel.findByIdAndDelete(req.params.id);
   if (!frame) {
     res.status(404).json("No frame was found");
@@ -83,6 +109,14 @@ function userIsLoggedIn(req) {
 async function userIsAuthor(req) {
   const frame = await FrameModel.findById(req.params.id);
   if (req.session.user._id == frame.user) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function userIsAdmin(req) {
+  if (req.session.user.role === "admin") {
     return true;
   } else {
     return false;
